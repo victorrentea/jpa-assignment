21668660d99af27a49cf447f1d709cd34101e5b8
victorrentea <victorrentea@gmail.com>
2022-09-08 11:36:10 +0300

    modul corect de a implem o @OneToMany unidirectioal + @JoinColumn cu nume fain

--- src/main/java/victor/training/jpa/entity/Teacher.java
+++ src/main/java/victor/training/jpa/entity/Teacher.java
@@ -44,6 +44,7 @@ public class Teacher {
 	//
 //	// TODO order by type and value ASC
 	@OneToMany
+	@JoinColumn(name = "TEACHER_ID")
 	private List<ContactChannel> channels = new ArrayList<>();
 //
 	@OneToMany(mappedBy = "holderTeacher")
